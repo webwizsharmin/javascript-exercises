dd34777f7cd99aad9c42e11c53c28c6eb5cb2e22
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2025-09-04 09:35:27 +0600

    complete the task

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
-
+console.log(reverseString("Hello world"));
 // Do not edit below this line
 module.exports = reverseString;
